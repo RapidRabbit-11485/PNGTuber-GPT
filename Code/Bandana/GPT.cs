5d8839fee8c9d776927f4b4406981e32132a8604
RapidRabbit11485 <chris@chrisbruscato.com>
2023-07-21 22:56:05 -0400

    Fixed bug with the Teach Action
    Fixed a bug where the bot would only check if keywords were mentioned in a prompt and didn't take into consideration the username of the user and whether something was remembered about them.

--- Code/Bandana/GPT.cs
+++ Code/Bandana/GPT.cs
@@ -15,15 +15,11 @@ public class CPHInline
     {
         try
         {
-            // Set the file paths for context and keyword-contexts
             string contextFilePath = args["CONTEXT_FILE_PATH"].ToString();
             string keywordContextFilePath = args["KEYWORD_FILE_PATH"].ToString();
-            // Read the username and raw input
             string userName = args["userName"].ToString();
             string rawInput = args["rawInput"].ToString();
-            // Read the context from a text file
             string context = File.ReadAllText(contextFilePath);
-            // Read the keyword-context mapping from a JSON file
             Dictionary<string, string> keywordContexts = new Dictionary<string, string>();
             if (File.Exists(keywordContextFilePath))
             {
@@ -31,12 +27,9 @@ public class CPHInline
                 keywordContexts = JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonContent) ?? new Dictionary<string, string>();
             }
 
-            // Set up the prompt with the provided context
             string prompt = userName + " asks " + rawInput;
-            // Check if any keywords are mentioned in the prompt
             var mentionedKeywords = keywordContexts.Keys;
             bool keywordMatch = false;
-            // Check if any mentioned keywords are exact matches in the prompt
             foreach (var keyword in mentionedKeywords)
             {
                 if (prompt.IndexOf(keyword, StringComparison.OrdinalIgnoreCase) >= 0)
@@ -49,33 +42,33 @@ public class CPHInline
                 }
             }
 
-            // Set up the prompt with the updated context
+            if (keywordContexts.ContainsKey(userName))
+            {
+                string usernamePhrase = $"something you know about {userName} is";
+                string usernameValue = keywordContexts[userName];
+                context += $"\n{usernamePhrase} {usernameValue}\n";
+            }
+
             string combinedContext = keywordMatch ? context : context;
 
-            // Log the combinedContext
             CPH.LogInfo("Combined Context: " + combinedContext);
 
-            // Specify the excluded categories for moderation
             string[] excludedCategories =
             {
                 "sexual",
                 "violence"
-            }; // Add the categories you want to exclude
-            // Call the moderation endpoint
+            };
             List<string> flaggedCategories = CallModerationEndpoint(prompt, excludedCategories);
-            // Check if any categories are flagged
             if (flaggedCategories.Except(excludedCategories).Any())
             {
                 string flaggedCategoriesString = string.Join(", ", flaggedCategories);
                 responseVariable = $"{userName} your request flagged for harmful or hateful content. Repeated attempts at abuse will result in a permanent ban.";
                 CPH.SetArgument("response", responseVariable);
                 CPH.LogInfo("Response: " + responseVariable);
-                return true; // Return early if the prompt is flagged
+                return true;
             }
 
-            // Execute the GPT Query and store the response in the variable
             GenerateChatCompletion(prompt, combinedContext).Wait();
-            // Set the response using the stored variable
             if (string.IsNullOrEmpty(responseVariable))
             {
                 responseVariable = "ChatGPT did not return a response.";
@@ -87,7 +80,6 @@ public class CPHInline
         }
         catch (Exception ex)
         {
-            // Log any exceptions that occur
             CPH.LogInfo("Exception: " + ex.ToString());
             return false;
         }
@@ -95,11 +87,9 @@ public class CPHInline
 
     public async Task GenerateChatCompletion(string prompt, string combinedContext)
     {
-        // Set up your OpenAI API credentials
         string apiKey = args["OPENAI_API_KEY"].ToString();
         CPH.LogInfo("API Key: " + apiKey);
         string completionsEndpoint = "https://api.openai.com/v1/chat/completions";
-        // Create the completions request payload
         var completionsRequest = new
         {
             model = "gpt-3.5-turbo",
@@ -117,37 +107,28 @@ public class CPHInline
                 }
             }
         };
-        // Serialize the completions request to JSON
         var completionsJsonPayload = JsonConvert.SerializeObject(completionsRequest);
         var completionsContentBytes = Encoding.UTF8.GetBytes(completionsJsonPayload);
-        // Create the completions request
         WebRequest completionsWebRequest = WebRequest.Create(completionsEndpoint);
         completionsWebRequest.Method = "POST";
         completionsWebRequest.Headers.Add("Authorization", $"Bearer {apiKey}");
         completionsWebRequest.ContentType = "application/json";
         completionsWebRequest.ContentLength = completionsContentBytes.Length;
-        // Write the completions request payload to the request stream
         using (Stream requestStream = completionsWebRequest.GetRequestStream())
         {
             requestStream.Write(completionsContentBytes, 0, completionsContentBytes.Length);
         }
 
-        // Send the completions request and get the response
         using (WebResponse completionsWebResponse = completionsWebRequest.GetResponse())
         {
-            // Read the completions response
             using (Stream responseStream = completionsWebResponse.GetResponseStream())
             {
                 using (StreamReader responseReader = new StreamReader(responseStream))
                 {
                     string completionsResponseContent = responseReader.ReadToEnd();
-                    // Log the completions response
                     CPH.LogInfo("Completions Response: " + completionsResponseContent);
-                    // Deserialize the response JSON for chat completions
                     var completionsJsonResponse = JsonConvert.DeserializeObject<ChatCompletionsResponse>(completionsResponseContent);
-                    // Extract the generated text from the response
                     string generatedText = completionsJsonResponse?.Choices?[0]?.Message?.content ?? string.Empty;
-                    // Check if generated text is empty
                     if (string.IsNullOrEmpty(generatedText))
                     {
                         responseVariable = "ChatGPT did not return a response.";
@@ -164,48 +145,37 @@ public class CPHInline
 
     public List<string> CallModerationEndpoint(string prompt, string[] excludedCategories)
     {
-        // Set up your OpenAI API credentials
         string apiKey = args["OPENAI_API_KEY"].ToString();
         string moderationEndpoint = "https://api.openai.com/v1/moderations";
-        // Set up the request payload for moderation
         var moderationRequestBody = new
         {
             input = prompt
         };
-        // Serialize the request payload for moderation
         var moderationJsonPayload = JsonConvert.SerializeObject(moderationRequestBody);
         var moderationContentBytes = Encoding.UTF8.GetBytes(moderationJsonPayload);
-        // Create the moderation request
         WebRequest moderationWebRequest = WebRequest.Create(moderationEndpoint);
         moderationWebRequest.Method = "POST";
         moderationWebRequest.Headers.Add("Authorization", $"Bearer {apiKey}");
         moderationWebRequest.ContentType = "application/json";
         moderationWebRequest.ContentLength = moderationContentBytes.Length;
-        // Write the moderation request payload to the request stream
         using (Stream requestStream = moderationWebRequest.GetRequestStream())
         {
             requestStream.Write(moderationContentBytes, 0, moderationContentBytes.Length);
         }
 
-        // Send the moderation request and get the response
         using (WebResponse moderationWebResponse = moderationWebRequest.GetResponse())
         {
-            // Read the moderation response
             using (Stream responseStream = moderationWebResponse.GetResponseStream())
             {
                 using (StreamReader responseReader = new StreamReader(responseStream))
                 {
                     string moderationResponseContent = responseReader.ReadToEnd();
-                    // Log the moderation response
                     CPH.LogInfo("Moderation Response: " + moderationResponseContent);
-                    // Deserialize the response JSON for moderation
                     var moderationJsonResponse = JsonConvert.DeserializeObject<ModerationResponse>(moderationResponseContent);
-                    // Check if any categories are flagged
                     List<string> flaggedCategories = moderationJsonResponse?.Results?[0]?.Categories
                         ?.Where(category => category.Value && !excludedCategories.Contains(category.Key))
                         .Select(category => category.Key)
                         .ToList() ?? new List<string>();
-                    // Log the flagged categories
                     CPH.LogInfo("Flagged Categories: " + string.Join(", ", flaggedCategories));
                     return flaggedCategories;
                 }
